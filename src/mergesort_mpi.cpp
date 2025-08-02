0a5ea53c2f713c4772e68afe56359dc7867c4ff1
Skiby7 <l.scoppitto@studenti.unipi.it>
2025-08-02 22:37:33 +0200

    debugging mpi

--- src/mergesort_mpi.cpp
+++ src/mergesort_mpi.cpp
@@ -27,6 +27,7 @@ int main(int argc, char *argv[]) {
 
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
+    std::cout << "Rank " << rank << std::endl;
     if (rank == 0) master(filename, size);
     else worker(TMP_LOCATION);
 
