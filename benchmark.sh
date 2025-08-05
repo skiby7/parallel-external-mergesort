4074eb7a4944a1b871b0cf095ba0dbf5ffe776ff
Skiby7 <l.scoppitto@studenti.unipi.it>
2025-08-05 19:53:00 +0200

    genfile

--- benchmark.sh
+++ benchmark.sh
@@ -3,18 +3,16 @@
 INPUT_FILE=$1
 PAYLOAD_SIZE=$2
 ITEMS_COUNT=$3
-if [ -z "$INPUT_FILE" ]; then
-    echo "Usage: $0 <input_file> [payload_size] [items_count]"
+if [ -z "$INPUT_FILE" ] || [ -z "$PAYLOAD_SIZE" ] || [ -z "$ITEMS_COUNT" ]; then
+    echo "Usage: $0 <input_file> <payload_size> <items_count>"
     echo "Specify SRUN=1 to schedule using slurm"
     exit 1
 fi
 
 MAIN_NODE="node01"
-# MAIN_NODE="node04"
 WORKER_NODES=(node02 node03 node04 node05 node06 node07 node08)
-# WORKER_NODES=(node05 node06 node07 node08)
 NODE_COUNTS=(2 3 4 5 6 7 8)
-# NODE_COUNTS=(2 4 8)
+
 
 if [ -n "$SRUN" ]; then
     SRUN="srun -w $MAIN_NODE"
@@ -25,28 +23,29 @@ fi
 $SRUN ls $INPUT_FILE
 INPUT_EXISTS=$?
 if [ $INPUT_EXISTS -eq 0 ]; then
-    echo "Input file already exists, to generate a new one, delete it first"
-else
-    if [ -z "$PAYLOAD_SIZE" ]; then
-        PAYLOAD_SIZE=64
-    fi
-    if [ -z "$ITEMS_COUNT" ]; then
-        ITEMS_COUNT=20000000
-    fi
-    echo "Generating input file with at most $PAYLOAD_SIZE bytes per item and $ITEMS_COUNT items..."
-    $SRUN make gen_file
-    $SRUN ./gen_file -r $PAYLOAD_SIZE -s $ITEMS_COUNT $INPUT_FILE
+    echo "Input file already exists, please delete it first"
 fi
 
+
+$SRUN make gen_file
+genFile() {
+    echo "Generating input file with at most $PAYLOAD_SIZE bytes per item and $ITEMS_COUNT items..."
+    $SRUN rm -f $INPUT_FILE
+    $SRUN ./gen_file -r $PAYLOAD_SIZE -s $ITEMS_COUNT $INPUT_FILE
+    FILESIZE=$($SRUN stat -c%s $INPUT_FILE)
+    ONE_THIRD_FILESIZE=$((FILESIZE/3)) # Simulating memory constraints
+    AVAIL_MEM=$($SRUN free -b | awk '/Mem:/ { print $7 }')
+    MAX_MEMORY=$((32 * 1024 * 1024 * 1024)) # 32GB
+    # Once set it is never updated to make sure that every test runs with the same memory constraints
+    if [ -z "$USABLE_MEM" ]; then
+      USABLE_MEM=$(
+        echo "$ONE_THIRD_FILESIZE $AVAIL_MEM $MAX_MEMORY" |
+        awk '{ min = $1; for (i = 2; i <= NF; i++) if ($i < min) min = $i; print min }'
+      )
+    fi
+}
+
 OUTPUT_FILE=$(dirname $INPUT_FILE)/output.dat
-FILESIZE=$($SRUN stat -c%s $INPUT_FILE)
-ONE_THIRD_FILESIZE=$((FILESIZE/3)) # Simulating memory constraints
-AVAIL_MEM=$($SRUN free -b | awk '/Mem:/ { print $7 }')
-MAX_MEMORY=$((32 * 1024 * 1024 * 1024)) # 32GB
-USABLE_MEM=$(
-  echo "$ONE_THIRD_FILESIZE $AVAIL_MEM $MAX_MEMORY" |
-  awk '{ min = $1; for (i = 2; i <= NF; i++) if ($i < min) min = $i; print min }'
-)
 TIMESTAMP=$(date +%s)
 LOG_FILE=run_${TIMESTAMP}.log
 
@@ -77,6 +76,7 @@ run_parallel() {
         echo -e "(nthreads=$i, max_mem=$USABLE_MEM)" | tee -a results/$LOG_FILE
         for j in $(seq 1 "$NRUNS")
         do
+            genFile
             $SRUN ./mergesort_omp -t $i -m $USABLE_MEM $INPUT_FILE | tee -a results/$LOG_FILE
             $SRUN /bin/rm $OUTPUT_FILE
             $SRUN ./mergesort_ff -t $i -m $USABLE_MEM $INPUT_FILE | tee -a results/$LOG_FILE
@@ -90,6 +90,8 @@ run_seq() {
     echo -e "(sequential binary merge, max_mem=$USABLE_MEM)" | tee -a results/$LOG_FILE
     for j in $(seq 1 "$NRUNS")
     do
+
+        genFile
         $SRUN ./mergesort_seq -m $USABLE_MEM $INPUT_FILE | tee -a results/$LOG_FILE
         $SRUN /bin/rm $OUTPUT_FILE
     done
@@ -98,6 +100,7 @@ run_seq() {
     echo -e "(sequential kway merge, max_mem=$USABLE_MEM)" | tee -a results/$LOG_FILE
     for j in $(seq 1 "$NRUNS")
     do
+        genFile
         $SRUN ./mergesort_seq -k -m $USABLE_MEM $INPUT_FILE | tee -a results/$LOG_FILE
         $SRUN /bin/rm $OUTPUT_FILE
     done
@@ -119,6 +122,7 @@ run_mpi_strong() {
         echo -e "(nnodes=$i, nthreads=$NTHREADS, max_mem=$USABLE_MEM)" | tee -a results/$LOG_FILE
         SRUN_MPI="srun --nodelist=${NODELIST} --ntasks-per-node=1 --mpi=pmix"
         for j in $(seq 1 $NRUNS); do
+            genFile
             $SRUN_MPI ./mergesort_mpi -t $NTHREADS -m $USABLE_MEM $INPUT_FILE | tee -a results/$LOG_FILE
             $SRUN /bin/rm -f $OUTPUT_FILE
         done
@@ -130,8 +134,7 @@ run_mpi_weak() {
     if [[ -z "$SRUN" ]]; then
         return 1
     fi
-    $SRUN cp $INPUT_FILE $INPUT_FILE.bk
-    $OG_INPUT_FILE="${INPUT_FILE}.bk"
+    INITIAL_ITEMS_COUNT=$ITEMS_COUNT
     LOG_FILE=run_${TIMESTAMP}_mpi_weak.log
     echo "#################################" | tee -a results/$LOG_FILE
     NTHREADS=32 # Using the best speedup from the single node version
@@ -145,11 +148,12 @@ run_mpi_weak() {
         echo -e "(nnodes=$i, filesize=$($SRUN stat -c%s $INPUT_FILE | tr -d '\n'), nthreads=$NTHREADS, max_mem=$USABLE_MEM)" | tee -a results/$LOG_FILE
         SRUN_MPI="srun --nodelist=${NODELIST} --ntasks-per-node=1 --mpi=pmix"
         for j in $(seq 1 $NRUNS); do
+            genFile
             $SRUN_MPI ./mergesort_mpi -t $NTHREADS -m $USABLE_MEM $INPUT_FILE | tee -a results/$LOG_FILE
             $SRUN /bin/rm -f $OUTPUT_FILE
         done
         # Increase the input size for each run
-        $SRUN bash -c "cat $OG_INPUT_FILE >> $INPUT_FILE"
+        ITEMS_COUNT=$((ITEMS_COUNT + INITIAL_ITEMS_COUNT))
     done
     echo "#################################" | tee -a results/$LOG_FILE
 }
