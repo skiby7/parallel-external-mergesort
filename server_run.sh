d45675dffcd764deb5cfe5c14a4ee45b71b4fbdc
Skiby7 <l.scoppitto@studenti.unipi.it>
2025-09-01 13:06:05 +0200

    closed

--- server_run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-./local_run.sh "/tmp/mergesort/test.dat" 16 20000000
-./local_run.sh "/tmp/mergesort/test.dat" 64 10000000
-./local_run.sh "/tmp/mergesort/test.dat" 512 1250000
-./local_run.sh "/tmp/mergesort/test.dat" 2048 312500
